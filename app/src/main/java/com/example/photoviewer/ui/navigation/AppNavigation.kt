6b8239680b9a832ee9d13f834235ee2d543482ca
Matixar <sylwester.kot99@gmail.com>
2024-10-28 18:55:22 +0100

    reformat code

--- app/src/main/java/com/example/photoviewer/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/photoviewer/ui/navigation/AppNavigation.kt
@@ -1,16 +1,7 @@
 package com.example.photoviewer.ui.navigation
 
 import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
